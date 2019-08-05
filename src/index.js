5c06b357d1f7538b78298986ecdcb4b65352d02d
mlobareva <mlobareva@yandex-team.ru>
2019-08-05 22:41:04 +0300

    initialized textarea with suggest

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/** React textarea with suggest v1.0.0
+ *
+ *
+ * Copyright (c) 2019-present,
+ * by Maria Lobareva (marialobareva97@gmail.com).
+ *
+ * LICENSE MIT.
+ */
+
+"use strict";
+
+import React from 'react';
+import Textarea from './Textarea.js';
+import "./styles.css";
+
+module.exports.textarea = Textarea;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack = require("webpack");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [],
+  entry: {
+    filename: path.resolve(__dirname, './src/') + '/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './umd/'),
+    filename: 'index.js',
+    libraryTarget: "umd"
+  }
+};
