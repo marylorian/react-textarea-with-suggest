4b7929346dbf26d48920ba89fdac075f1a636946
mlobareva <mlobareva@yandex-team.ru>
2019-08-07 21:52:24 +0300

    new version of babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,4 @@
 module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
-  plugins: [
-    ["@babel/plugin-proposal-class-properties", { "loose": true }]
-  ]
+  plugins: ["@babel/plugin-proposal-class-properties", "@babel/plugin-transform-arrow-functions"]
 };
