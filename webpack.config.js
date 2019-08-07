eb38ddf77af18b830027a5647a044c07eca9c909
mlobareva <mlobareva@yandex-team.ru>
2019-08-07 22:22:52 +0300

    new version of babel config

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
           options: {
             presets: ['@babel/preset-env',
               '@babel/preset-react', {
-                'plugins': ['@babel/plugin-proposal-class-properties']
+                'plugins': [['@babel/plugin-proposal-class-properties', {loose: true}]]
               }]
           }
         }
