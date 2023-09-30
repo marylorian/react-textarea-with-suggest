08561545c84d8ed34c1426baa936099c9ea2a904
marylorian <marialobareva97@gmail.com>
2023-09-30 15:12:30 +0200

    updates builded lib, updates pre-push hook

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,3 @@
 import { TextareaWithSuggests } from "./components/TextareaWithSuggests";
-export { AutoHighlightFirstItemValues } from './constants';
+export { AutoHighlightFirstItemValues } from "./constants";
 export default TextareaWithSuggests;
